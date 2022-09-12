89166999039558f3a7effe363deabd9efd58f74d
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-09-13 09:56:19 +1000

    PC_Work commit

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
@@ -5,6 +6,7 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private float _speed;
 
     private Animator _animator;
+    private bool _isDirectionRight = true;
 
     private void Start()
     {
@@ -23,13 +25,27 @@ public class PlayerAnimation : MonoBehaviour
         {
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
             _animator.SetTrigger("MoveLeft");
-            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            if (_isDirectionRight)
+            {
+                TurnDirection();
+                _isDirectionRight = false;
+            }
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             _animator.SetTrigger("MoveRight");
             transform.Translate(_speed * Time.deltaTime, 0, 0);
+            if (!_isDirectionRight)
+            {
+                TurnDirection();
+                _isDirectionRight = true;
+            }
         }
+    }
+
+    private void TurnDirection()
+    {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
     }
 }
