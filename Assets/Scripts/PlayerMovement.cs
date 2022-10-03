9ef0b6636648cfb7efdfac668d458a0afd830381
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-10-04 09:39:44 +1000

    PC_Home commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private IsGroundedTrigger _isGrounded;
 
+    private Rigidbody2D _rigidbody;
     private Animator _animator;
     private bool _isDirectionRight = true;
+    private float _verticalSpeed;
+
+    private float _sphereRadius = 1f;
+    private bool _isGrounded;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
-        _isGrounded.PlayerUngrounded.AddListener(Jump);
-        _isGrounded.PlayerGrounded.AddListener(Jump);
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _verticalSpeed = _rigidbody.velocity.magnitude;
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && _verticalSpeed == 0)
         {
-            _animator.SetTrigger("Jump");
-            transform.Translate(0, _speed * Time.deltaTime, 0);
-            Jump();
+            _isGrounded = Physics.CheckSphere(transform.position, _sphereRadius);
+
+            if (_isGrounded)
+            {
+                _animator.SetTrigger("Jump");
+                transform.Translate(0, _speed * Time.deltaTime, 0);
+            }
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -52,10 +60,5 @@ public class PlayerMovement : MonoBehaviour
     private void TurnDirection()
     {
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-    }
-
-    private void Jump()
-    {
-
     }
 }
